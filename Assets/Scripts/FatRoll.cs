684ce8c9c3fb2ab8f5cc5cdc8b8dec1f8b1ed372
RobinHecate <124266311+RobinHecate@users.noreply.github.com>
2023-05-24 16:03:28 +0200

    Fat Roll

--- Assets/Scripts/FatRoll.cs
+++ Assets/Scripts/FatRoll.cs
@@ -7,23 +7,25 @@ public class FatRoll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeanTween.init(800);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.instance.biberonGiven)
-        {
+        //if (GameManager.instance.biberonGiven)
+        //{
             StartCoroutine(rollingStones());
-        }
+        //}
     }
 
     IEnumerator rollingStones()
     {
         yield return new WaitForSeconds(1f);
-        LeanTween.moveZ(gameObject, -50, 1).setEaseOutSine();
-        LeanTween.rotateZ(gameObject, 90, .3f);
-        LeanTween.rotateX(gameObject, -360, 1f).setEaseOutSine();
+        LeanTween.moveZ(gameObject, -55, 3).setDelay(.4f);
+        LeanTween.rotateZ(gameObject, -180, 3).setDelay(.4f);
+        LeanTween.rotateX(gameObject, 0, .3f);
+        LeanTween.moveY(gameObject, 2.5f, .3f);
+        
     }
 }
